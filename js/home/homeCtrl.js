ef4756213c8cee6af83b3dee1bb8ac53b37432c0
Mark McIver <markmciver83@gmail.com>
2015-06-08 18:11:43 -0600

    first time using .all()

--- js/home/homeCtrl.js
+++ js/home/homeCtrl.js
@@ -1,5 +1,32 @@
 var app = angular.module('nbaRoutes');
 
-app.controller('homeCtrl', function($scope, homeService){
+app.controller('homeCtrl', function($scope, $q, teamService, teamData){
+
+	$scope.teamData = teamData;
+	console.log($scope.teamData)
+
+	for(var i = 0; i < $scope.teamData.length; i++){
+		if($scope.teamData[i][0].homeTeam === 'utahjazz'){
+			$scope.teamData[i].homeTeam = 'Utah Jazz',
+			$scope.teamData[i].logoPath = '../images/jazz-logo.png';
+		} else if($scope.teamData[i][0].homeTeam === 'losangeleslakers'){
+			$scope.teamData[i].homeTeam = 'Los Angeles Lakers';
+			$scope.teamData[i].logoPath = '../images/lakers-logo.png';
+		} else if($scope.teamData[i][0].homeTeam === 'miamiheat'){
+			$scope.teamData[i].homeTeam = 'Miami Heat';
+			$scope.teamData[i].logoPath = '../images/heat-logo.png';
+		}
+	}
+	
+	/*if($routeParams.team === 'utahjazz'){
+			$scope.homeTeam = 'Utah Jazz';
+			$scope.logoPath = '../images/jazz-logo.png';
+		} else if($routeParams.team === 'losangeleslakers'){
+			$scope.homeTeam = 'Los Angeles Lakers';
+			$scope.logoPath = '../images/lakers-logo.png';
+		} else if ($routeParams.team === 'miamiheat'){
+			$scope.homeTeam = 'Miami Heat';
+			$scope.logoPath = '../images/heat-logo.png';
+		}*/
 
 });
--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -2,4 +2,50 @@ var app = angular.module('nbaRoutes');
 
 app.service('teamService', function($http, $q){
 
+	this.addNewGame = function(gameObj){
+		var url = 'https://api.parse.com/1/classes/' + gameObj.homeTeam;
+		if(parseInt(gameObj.homeTeamScore) > parseInt(gameObj.opponentScore)){
+			gameObj.won = true;
+		} else {
+			gameObj.won = false;
+		};
+
+		 return $http({
+			method: 'POST',
+			url: url,
+			data: gameObj
+		});
+	}
+
+	this.getTeamData = function(team){
+		var dfd = $q.defer();
+		var url = 'https://api.parse.com/1/classes/' + team;
+		$http({
+			method: 'GET',
+			url: url
+		}).then(function(response){
+			var results = response.data.results;
+			var wins = 0;
+			var losses = 0;
+			for(var i = 0; i < results.length; i++){
+				if(results[i].won === true){
+					wins++;
+				} else{
+					losses++;
+				}
+			}
+			results.wins = wins;
+			results.losses = losses;
+			dfd.resolve(results);
+		})
+		return dfd.promise;
+	}
+
+	this.getAllData = function(){
+		var promises = [this.getTeamData('utahjazz'), this.getTeamData('losangeleslakers'), this.getTeamData('miamiheat')];
+		console.log(promises);
+		return $q.all(promises);
+	}
+
+
 });
